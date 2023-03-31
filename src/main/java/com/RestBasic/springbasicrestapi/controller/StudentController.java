127bff3b133ad5bb921226d7b294ddc482808b17
xuanphong01bn <donhatphong94@gmail.com>
2023-03-31 22:03:31 +0700

    use Responsity class to custome api result

--- src/main/java/com/RestBasic/springbasicrestapi/controller/StudentController.java
+++ src/main/java/com/RestBasic/springbasicrestapi/controller/StudentController.java
@@ -1,21 +1,64 @@
 package com.RestBasic.springbasicrestapi.controller;
 
 import com.RestBasic.springbasicrestapi.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StudentController {
 
     @GetMapping("/student")
-    public Student getStudent(){
+    public ResponseEntity<Student>  getStudent(){
         Student student = new Student(
                 1,
                 "Phong",
                 "Nguyen"
         );
-        return student;
+//        return new ResponseEntity<>(student, HttpStatus.OK);
+        return ResponseEntity.ok().header("custome-header","Phong")
+                .body(student);
     }
 
+
+    @GetMapping("/students")
+    public List<Student> getListStudent(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(2,"Theo","Silva"));
+        students.add(new Student(3,"Jordan","Berberin"));
+
+        return students;
+    }
+
+    //Spring boot rest with path variable
+
+    @GetMapping("/students/{id}")
+    public Student studentPathVaribale(@PathVariable("id")  int studentId) {
+        return new Student(studentId, "Phong 1", "Nguyen");
+    }
+
+    //Request Params
+//    http://localhost:8080/student/query?id=0&firstName=urigan&lastName=tai
+    @GetMapping("/student/query")
+    public Student studentRequestParams(@RequestParam int id,
+                                        @RequestParam String firstName,
+                                        @RequestParam String lastName
+
+                                        ){
+        return new Student(id, firstName, lastName);
+    }
+
+    //Post req with params
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student) {
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
+    }
 
 }
